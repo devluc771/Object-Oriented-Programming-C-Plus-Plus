ffd687108e65bedb60ebb89bf73fba8612cd8c47
devluc771 <devluc771@gmail.com>
2025-09-11 15:44:50 -0700

    Update Hello.cpp
    Added code for Hello.cpp

--- Lab1/Hello.cpp
+++ Lab1/Hello.cpp
@@ -1,1 +1,14 @@
+/***
+ *  Purpose of this lab is to get used to Github/VSCode
+ *  This program prints Hello World and Hello World Again on the next line
+ ***/
 
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    printf("Hello World Again!\n");
+    return 0;
+}
