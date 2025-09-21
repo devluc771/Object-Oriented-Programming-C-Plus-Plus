184f1289f359d060478dd5f04cc3b72a65927c8f
devluc771 <devluc771@gmail.com>
2025-09-20 21:56:06 -0700

    Finished code for main.cpp

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -8,5 +8,17 @@ int main()
     RPG p1 = RPG("Wiz", 0, 0.2, 60, 1);
     RPG p2 = RPG();
 
-    printf("%s Current")
+    printf("%s Current Stats\n", p1.getName().c_str());
+    printf("Hits Taken: %i\t Luck: %f\t Exp: %f\t Level: %i\n", p1.getHitsTaken(), p1.getLuck(), p1.getExp(), p1.getLevel());
+    printf("%s Current Stats\n", p2.getName().c_str());
+    printf("Hits Taken: %i\t Luck: %f\t Exp: %f\t Level: %i\n", p2.getHitsTaken(), p2.getLuck(), p2.getExp(), p2.getLevel());
+
+    p2.setHitsTaken(3);
+    printf("\n%s hits taken: ", p2.getName().c_str());
+    cout << p2.getHitsTaken();
+    cout << "\n0 is dead, 1 is alive\n";
+    p1.isAlive();
+    p2.isAlive();
+
+    return 0; 
 }
